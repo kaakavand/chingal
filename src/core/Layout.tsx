da400e9d4357b887634d928696c49fb29466caa9
Amirhossein Kakavand <kakavand2k@gmail.com>
2023-10-28 16:50:32 +0330

    add logo svg

--- src/core/Layout.tsx
+++ src/core/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import logo from './../assets/images/chingal-logo.svg'
 interface propsType {
     children : any
 }
@@ -7,15 +7,16 @@ interface propsType {
 const Layout = ({children} : propsType) => {
   return (
     <>
-        <header className='bg-surface-100  border-b-2 border-surface-300 py-4'>
-            {/* <figure> */}
-                {/* <img src={} alt="logo" /> */}
-            {/* </figure>
+        <header className='bg-surface-100 border-b-2 border-surface-300 py-4'>
+            <div className="container flex justify-between  mx-auto">
+            <figure className='w-40'>
+                <img src={logo} alt="logo" />
+            </figure>
             <div>
                 <input className='p-1 rounded-full' type="text" />
                 <button>Dark</button>
-            </div> */}
-            adfasdf
+            </div>
+            </div>
         </header>
         <div className='container mx-auto'>
             {children}
